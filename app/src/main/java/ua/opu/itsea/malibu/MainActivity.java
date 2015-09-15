5b3adbed67274abd21d631b80e059a25bdfac895
NickGodov <nick1985>
2015-09-15 20:00:11 +0300

    auth

--- app/src/main/java/ua/opu/itsea/malibu/MainActivity.java
+++ app/src/main/java/ua/opu/itsea/malibu/MainActivity.java
@@ -23,20 +23,24 @@
 package ua.opu.itsea.malibu;
 
 import android.app.Activity;
-import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.os.Handler;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.dd.CircularProgressButton;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 import ua.opu.itsea.malibu.utils.Notifications;
 
@@ -46,16 +50,20 @@ public class MainActivity extends Activity {
     EditText mLogin;
     EditText mPassword;
 
+    String remoteURL = "http://malibu.easyfitapp.net/api/public/clubs";
+    String login = "quest";
+    String pass = "qwerty";
+
     FrameLayout mLayout;
     ImageView mLoginScrenBackground;
 
     // Список изображений, одно из которых выбирается для фона
     int[] mBackgroundsList =
             {R.drawable.bg_aerobics,
-             R.drawable.bg_aqua,
-             R.drawable.bg_gym,
-             R.drawable.bg_cycle,
-             R.drawable.bg_dance};
+                    R.drawable.bg_aqua,
+                    R.drawable.bg_gym,
+                    R.drawable.bg_cycle,
+                    R.drawable.bg_dance};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,10 +82,10 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 if (circularButton1.getProgress() == 0) {
                     circularButton1.setProgress(50);
-                    new AuthenticationTask().execute();
+                    new AuthenticationTask().execute(mLogin.getText().toString(), mPassword.getText().toString());
                 } else if (circularButton1.getProgress() == -1) {
                     circularButton1.setProgress(0);
-                } else if (circularButton1.getProgress() == 100){
+                } else if (circularButton1.getProgress() == 100) {
                     Intent subActivity = new Intent(MainActivity.this,
                             ScanActivity.class);
                     startActivity(subActivity);
@@ -107,41 +115,55 @@ public class MainActivity extends Activity {
         mLoginScrenBackground.setImageResource(mBackgroundsList[random]);
     }
 
-    class AuthenticationTask extends AsyncTask<Void, Boolean, Void> {
-        protected void onProgressUpdate(Boolean... params) {
-            if (params[0]) {
+
+    private static URLConnection setUpBasicConnection(String uri, String name, String pass) throws IOException {
+        URL url = new URL(uri);
+        URLConnection uc = url.openConnection();
+        String userpass = name + ":" + pass;
+        String basicAuth = "Basic " + toBase64(userpass.getBytes());
+        uc.setRequestProperty("Authorization", basicAuth);
+        uc.setRequestProperty("Content-Type", "application/json");
+
+        return uc;
+    }
+
+    public static String toBase64(byte[] bytes) {
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
+
+    class AuthenticationTask extends AsyncTask<String, Integer, Void> {
+        protected void onProgressUpdate(Integer... params) {
+            if (params[0] >= 200 && params[0] <= 299) {
                 circularButton1.setProgress(100);
             } else {
                 circularButton1.setProgress(-1);
-                Notifications.showErrorNotification(mLayout, getString(R.string.error_no_internet_connection));
+//                Notifications.showErrorNotification(mLayout, getString(R.string.error_no_internet_connection));
             }
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Void doInBackground(String... params) {
+            int responseCode = 0;
             try {
-                Thread.sleep(2000);
-            } catch (Exception e) {
+                URLConnection conn = setUpBasicConnection(remoteURL, params[0], params[1]);
+                conn.connect();
+                responseCode = ((HttpURLConnection)conn).getResponseCode();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-            if (checkInternetConnection()) {
-                publishProgress(true);
-            } else {
-                publishProgress(false);
-            }
+            publishProgress(responseCode);
             return null;
         }
+
+        /**
+         * Проверка интернет-соединения
+         *
+         * @return наличие интернет-соединения
+         */
+        private boolean checkInternetConnection() {
+            ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            return netInfo != null && netInfo.isConnectedOrConnecting();
+        }
     }
-
-    /**
-     * Проверка интернет-соединения
-     * @return наличие интернет-соединения
-     */
-    private boolean checkInternetConnection() {
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        return netInfo != null && netInfo.isConnectedOrConnecting();
-    }
-
-
 }
